752eabd776b934df46a57262c0c60e98b6cc7ea1
svist5 <website.svist@gmail.com>
2023-07-01 20:09:08 +0530

    changes

--- models/tokenModel.js
+++ models/tokenModel.js
@@ -1,11 +1,11 @@
 const mongoose=require("mongoose");
 const tokenSchema=mongoose.Schema({
-    name:{default:"HareKrishna"},
+    name:{type:String,default:"HareKrishna"},
     token:{type:String,default:"undefined"}
 
 },{
 timestamps:true,
 }
 )
-const Token=mongoose.model("Token",postSchema);
+const Token=mongoose.model("Token",tokenSchema);
 module.exports=Token;
